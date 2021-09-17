7775ba1dfb048f3fbb462ef2ce6df7fc57da29b1
timstetter <tstetter@gmail.com>
2021-09-17 09:53:36 -0400

    finished facebook OAuth setup

--- models/user.js
+++ models/user.js
@@ -11,6 +11,10 @@ const userSchema = new Schema({
     type: String,
     default: "",
   },
+  facebookId: {
+    type: String,
+    default: "",
+  },
   admin: {
     type: Boolean,
     default: false,
